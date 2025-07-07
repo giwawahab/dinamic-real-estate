5b713b434c7621b5462553a9e480cc9d45d9b618
Giwa Wahab Tomiwa <giwawahabtomiwa2022@gmail.com>
2025-07-07 04:19:28 +0100

    Multi Auth login system completed

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-// use View;
 
 class AdminAuthController extends Controller
 {
     public function login(): View
     {
         return view('admin.auth.login');
+    }
+
+    public function passwordRequest(): View
+    {
+        return view('admin.auth.forgot-password');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminAuthController;
-use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin/login', [AdminAuthController::class, 'login'])->name('admin.login');
-Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index')->middleware('user.type:admin');
 
 Route::get('/', function () {
     return view('welcome');
