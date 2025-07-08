ad99bf1f931a0af3f8f30d92f2f1ca6a43ef8071
Giwa Wahab Tomiwa <giwawahabtomiwa2022@gmail.com>
2025-07-08 22:37:51 +0100

    Admin dashboard, login and forgot password design completed

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,7 +8,8 @@ use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-    function index(): View {
+    function index(): View
+    {
 
         return view('admin.dashboard.index');
     }
